d58887ad71ce5f2982a69a3d449814241694c4bd
Azaj <azajasari77@gmail.com>
2022-09-29 00:17:41 +0530

    submit

---
+++ assignment_8/Q_3.py
@@ -0,0 +1,11 @@
+#3. Write a python script to print first 10 natural numbers in reverse order
+
+# i=1
+# while i<=11:
+#     print(11-i, " ",end='')
+#     i=i+1
+
+i=10
+while i>=1:
+    print(i," ",end="")    
+    i=i-1
