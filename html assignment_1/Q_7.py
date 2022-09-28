3931de6c09e52b1a25114e9f9392b5d20b2f4817
Azaj <azajasari77@gmail.com>
2022-09-28 11:42:43 +0530

    submit

---
+++ html assignment_1/Q_7.py
@@ -0,0 +1,8 @@
+#7. Write a python script to calculate average of three numbers, entered by the user
+
+A = float(input("Enter first number"))
+B = float(input("Enter second number"))
+C = float(input("Enter third number"))
+
+Average = (A+B+C)/3
+print(Average)
